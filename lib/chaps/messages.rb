6f856a0c8835557cbd24073410fa639210d12b16
Tom Lea <contrib@tomlea.co.uk>
2008-12-27 19:34:19 +0000

    Auth working.
    Can be tested with:
      echo -e 'A0test\nA1d41d8cd98f00b204e9800998ecf8427e\n' | nc localhost 10001

--- lib/chaps/messages.rb
+++ lib/chaps/messages.rb
@@ -12,6 +12,7 @@ module Chaps
     
     module Outbound
       class A0
+        attr_reader :random_data
         def initialize(random_data)
           @random_data = random_data
           raise Exception, "Random Data Should be 50 Chars" unless @random_data.length == 50
@@ -21,13 +22,34 @@ module Chaps
           "A0#{@random_data}\n"
         end
       end
+
+      class A1
+        def to_s
+          "A1U\n"
+        end
+      end
+      
+      class ER
+        def initialize(code)
+          @code = code
+        end
+        
+        def to_s
+          "ER#{@code}\n"
+        end
+      end
+      
+      module Errors
+        BadUsername = ER.new("000")
+        BadPassword = ER.new("002")
+      end
     end
     
     module Inbound
       class A0
         attr_reader :username, :client, :protocol_version
         def initialize(message)
-          if matches = message.match(/^..([^\t]+)(?:\t(.*)([0-9]))?$/)
+          if matches = message.match(/^([^\t]+)(?:\t(.*)([0-9]))?$/)
             _, @username, @client, @protocol_version = matches.to_a
             @protocol_version = @protocol_version.to_i
           else
@@ -39,7 +61,7 @@ module Chaps
       class A1
         attr_reader :md5
         def initialize(message)
-          if matches = message.match(/^..([a-z0-9]{32})$/)
+          if matches = message.match(/^([a-f0-9]{32})$/)
             _, @md5 = matches.to_a
           else
             raise Exception, "Bad Message"
--- lib/chaps/server.rb
+++ lib/chaps/server.rb
@@ -1,14 +1,58 @@
 require 'gserver'
+require 'yaml'
+require 'digest/md5'
+
 module Chaps
   class Server < GServer
     def initialize(port=10001, *args)
       super(port, *args)
     end
+    
     def serve(io)
+      if authenticate(io)
+        io << "So you managed to authenticate your client, I hope you're happy now!\n"
+      end
+    rescue => e
+      puts "#{e.class}: #{e.message}"
+      puts e.backtrace
+    raise
+    end
+    
+    def authenticate(io)
+      message = expect(io, :A0)
+
+      salt = "a"*50
+      io << Messages::Outbound::A0.new(salt)
+      
+      username = message.username
+      password = password_for(username)
+      
+      auth = expect(io, :A1)
+      
+      if password.nil?
+        io << Messages::Outbound::Errors::BadUsername
+        return false
+      else
+        if auth.md5 == Digest::MD5.hexdigest(salt + password)
+          io << Messages::Outbound::A1.new
+          return true
+        else
+          io << Messages::Outbound::Errors::BadPassword
+          return false
+        end
+      end
+    end
+    
+    def password_for(username)
+      return "test" if username == "test"
+      YAML.load(File.read("auth.yml"))[username] rescue nil
+    end
+    
+    def expect(io, klass_name)
+      raise Exception, "No such message as an #{klass_name}" unless Messages::Inbound.const_defined?(klass_name)
       message = Messages.parse(io.gets)
-      raise "Expected A0" unless message.is_a? Messages::Inbound::A0
-      response = Messages::Outbound::A0.new("a"*50)
-      io << response
+      raise Exception, "Expected a #{klass_name}, got a #{message.class.name}" unless message.is_a? Messages::Inbound.const_get(klass_name)
+      message
     end
   end
 end
